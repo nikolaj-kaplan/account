82965440d39931093c390b3bf7df84c16770e740
Nikolaj Hjelm Kaplan <nikolaj.kaplan@gmail.com>
2013-03-25 22:04:53 +0100

    raven

--- Account/Controllers/HomeController.cs
+++ Account/Controllers/HomeController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Raven.Client.Document;
+using Raven.Client.Embedded;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,8 +10,15 @@ namespace Account.Controllers
 {
     public class HomeController : Controller
     {
+        private static DocumentStore _documentStore;
         public ActionResult Index()
         {
+            if (_documentStore == null)
+            {
+                _documentStore = new EmbeddableDocumentStore { ConnectionStringName = "Local" };
+                _documentStore.Initialize();
+            }
+
             ViewBag.Message = "Modify this template to jump-start your ASP.NET MVC application.";
 
             return View();
@@ -19,12 +28,30 @@ namespace Account.Controllers
         {
             ViewBag.Message = "Your app description page.";
 
+
+            Random r = new Random();
+
+            using (var session = _documentStore.OpenSession())
+            {
+                session.Store(new TestClass() { A = r.Next(100), B = r.Next(100) });
+                session.SaveChanges();
+            }
+
+
             return View();
         }
 
         public ActionResult Contact()
         {
-            ViewBag.Message = "Your contact page.";
+            ViewBag.Message = "Your contact page.<br />";
+            using (var session = _documentStore.OpenSession())
+            {
+                var objects = session.Query<TestClass>();
+                foreach (TestClass c in objects)
+                {
+                    ViewBag.Message += c.ToString() + "<br />";
+                }
+            }
 
             return View();
         }
---
+++ Account/Controllers/TestClass.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Account.Controllers
+{
+    class TestClass
+    
+    {
+        public string Id { get; set; }
+
+        public int A { get; set; }
+        public int B { get; set; }
+
+        public override string ToString()
+        {
+            return "A: " + A + ", B: " + B;
+        }
+    }
+}
